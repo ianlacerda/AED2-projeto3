f7fde00892775568aa247da539bf759daef86032
gvilherme <rodrigues.guilherme@aluno.ufabc.edu.br>
2021-04-26 22:22:20 -0300

    added main and fixed imports

--- AED2_projeto3/simulation/simulation.py
+++ AED2_projeto3/simulation/simulation.py
@@ -1,20 +1,21 @@
-from ..cache.cache import Cache
+from AED2_projeto3.cache.cache import Cache
 
-"""Start a new Cache"""
-cache = Cache()
+def run_simulation():
+    """Start a new Cache"""
+    cache = Cache()
 
-"""Read requisitions from data folder"""
-data = open(f'/home/griquup/ufabc/projeto3/AED2_projeto3/AED2_projeto3/data/wikipedia.txt', 'r')
-data = data.readlines()
+    """Read requisitions from data folder"""
+    data = open(f'/home/griquup/ufabc/projeto3/AED2_projeto3/AED2_projeto3/data/wikipedia.txt', 'r')
+    data = data.readlines()
 
-"""Insert requisitions into cache"""
-for line in data:
-    cache.req(req_val=line.strip())
+    """Insert requisitions into cache"""
+    for line in data:
+        cache.req(req_val=line.strip())
 
-"""print loaded bytes"""
-print(cache._loaded_bytes)
+    """print loaded bytes"""
+    print(cache._loaded_bytes)
 
-"""print saved bytes"""
-print(cache._saved_bytes)
+    """print saved bytes"""
+    print(cache._saved_bytes)
 
-#print(cache._cache)
+    #print(cache._cache)
